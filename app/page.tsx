2835a58b03ac805228d00fc23f982b4c46c00837
muhammedsafuvan <muhammedsafuvan024@gmail.com>
2024-11-12 22:10:54 +0530

    fix: remove pet table usage

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,16 @@
-// import Image from 'next/image'
-// import Link from 'next/link'
-// import ProductCard from './components/ProductCard'
+import Image from 'next/image'
+import Link from 'next/link'
+import ProductCard from './components/ProductCard'
 
-// export default function Home() {
-//   return (
-//     <main>
-//       <h1>Hello World!</h1>
-      
-//       <a href="/users">Users</a>
-//       <Link href="/users" > link-user</Link>
-//       <ProductCard />
-
-//     </main>
-//   )
-// }
-
-import { sql } from "@vercel/postgres";
-
-export default async function Cart() {
-  const { rows } = await sql`SELECT * from PETS where name=${'raju'}`;
-
+export default function Home() {
   return (
-    <div>
-      {rows.map((row) => (
-        <div key={row.name}>
-          {row.name} - {row.owner}
-        </div>
-      ))}
-    </div>
-  );
+    <main>
+      <h1>Hello World!</h1>
+      
+      <a href="/users">Users</a>
+      <Link href="/users" > link-user</Link>
+      <ProductCard />
+
+    </main>
+  )
 }
