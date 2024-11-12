ee8cb0e85a2c68ade3cd3dbf2bffa56e6e314f79
muhammedsafuvan <muhammedsafuvan024@gmail.com>
2024-11-12 22:03:05 +0530

    Add relation between User and Todo models

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,32 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import ProductCard from './components/ProductCard'
+// import Image from 'next/image'
+// import Link from 'next/link'
+// import ProductCard from './components/ProductCard'
 
-export default function Home() {
-  return (
-    <main>
-      <h1>Hello World!</h1>
+// export default function Home() {
+//   return (
+//     <main>
+//       <h1>Hello World!</h1>
       
-      <a href="/users">Users</a>
-      <Link href="/users" > link-user</Link>
-      <ProductCard />
+//       <a href="/users">Users</a>
+//       <Link href="/users" > link-user</Link>
+//       <ProductCard />
 
-    </main>
-  )
+//     </main>
+//   )
+// }
+
+import { sql } from "@vercel/postgres";
+
+export default async function Cart() {
+  const { rows } = await sql`SELECT * from PETS where name=${'raju'}`;
+
+  return (
+    <div>
+      {rows.map((row) => (
+        <div key={row.name}>
+          {row.name} - {row.owner}
+        </div>
+      ))}
+    </div>
+  );
 }
